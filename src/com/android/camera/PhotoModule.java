CyanogenMod/android_packages_apps_Gallery2

Gallery2: fix HDR crashes && fix exposure from being set.
* HDR transcoding is done on a different thread. got rid of the
stall in the transcooding step and possibliy fix race conditions.
* lock out the exposure step from being set, also fix an issue
of setting is being set by a method that is called on idle.

Change-Id: I9558558777a11cc8f6b633e397c809aa6993ceaf

src/com/android/camera/PhotoModule.java
@@ -256,8 +256,6 @@ public class PhotoModule
     private final Handler mHandler = new MainHandler();
     private PreferenceGroup mPreferenceGroup;
 
-    private int mResetExposure;
-
     private boolean mQuickCapture;
 
     CameraStartUpThread mCameraStartUpThread;
@@ -464,7 +462,6 @@ public class PhotoModule
 
     private void onPreviewStarted() {
         mCameraStartUpThread = null;
-        mResetExposure = mParameters.getExposureCompensation();
         setCameraState(IDLE);
         if (!ApiHelper.HAS_SURFACE_TEXTURE) {
             // This may happen if surfaceCreated has arrived.
@@ -797,7 +794,7 @@ public class PhotoModule
 
         @Override
         public void onPictureTaken(
-                byte [] jpegData, final android.hardware.Camera camera) {
+                final byte [] jpegData, final android.hardware.Camera camera) {
             if (mPaused) {
                 return;
             }
@@ -865,8 +862,8 @@ public class PhotoModule
             if (!mIsImageCaptureIntent) {
                 // Calculate the width and the height of the jpeg.
                 Size s = mParameters.getPictureSize();
-                ExifInterface exif = Exif.getExif(jpegData);
-                int orientation = Exif.getOrientation(exif);
+                final ExifInterface exif = Exif.getExif(jpegData);
+                final int orientation = Exif.getOrientation(exif);
                 int width, height;
                 if ((mJpegRotation + orientation) % 180 == 0 ||
                         (mSceneMode == Util.SCENE_MODE_HDR &&
@@ -877,7 +874,7 @@ public class PhotoModule
                     width = s.height;
                     height = s.width;
                 }
-                String title = mNamedImages.getTitle();
+                final String title = mNamedImages.getTitle();
                 long date = mNamedImages.getDate();
                 if (title == null) {
                     Log.e(TAG, "Unbalanced name/data pair");
@@ -896,29 +893,42 @@ public class PhotoModule
                     }
 
                     if (isSamsungHDR) {
-                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                        Bitmap bm = Util.decodeYUV422P(jpegData, width, height);
-                        if (mJpegRotation != 0) {
-                            Matrix matrix = new Matrix();
-                            matrix.postRotate(mJpegRotation);
-                            bm = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, true);
-                        }
-                        if (mJpegRotation % 180 != 0) {
-                            int x = height;
-                            int y = width;
-                            width = x;
-                            height = y;
-                        }
+                        final int heights = height;
+                        final int widths = width;
+                        final long dates = date;
+                        new Thread(new Runnable() {
+                            public void run() {
+                                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                                Bitmap bm = Util.decodeYUV422P(jpegData, widths, heights);
+                                if (mJpegRotation != 0) {
+                                    Matrix matrix = new Matrix();
+                                    matrix.postRotate(mJpegRotation);
+                                    bm = Bitmap.createBitmap(bm, 0, 0, widths, heights, matrix, true);
+                                }
+                                int heights1 = heights;
+                                int widths1 = widths;
+                                if (mJpegRotation % 180 != 0) {
+                                    int x =  heights;
+                                    int y =  widths;
+                                    widths1 = x;
+                                    heights1 = y;
+                                }
 
-                        bm.compress(Bitmap.CompressFormat.JPEG,
-                                    CameraSettings.getJpegQualityIntValue(mPreferences),
-                                    baos);
+                                bm.compress(Bitmap.CompressFormat.JPEG,
+                                            CameraSettings.getJpegQualityIntValue(mPreferences),
+                                            baos);
 
-                        jpegData = baos.toByteArray();
-                    }
-                    mActivity.getMediaSaveService().addImage(
+                                mActivity.getMediaSaveService().addImage(
+                                    baos.toByteArray(), title, dates, mLocation, widths1, heights1,
+                                    orientation,  exif, mOnMediaSavedListener, mContentResolver);
+                            }
+                        }).start();
+
+                    } else {
+                        mActivity.getMediaSaveService().addImage(
                             jpegData, title, date, mLocation, width, height,
                             orientation, exif, mOnMediaSavedListener, mContentResolver);
+                    }
                 }
             } else {
                 mJpegImageData = jpegData;
@@ -941,9 +951,6 @@ public class PhotoModule
                     + mJpegCallbackFinishTime + "ms");
             mJpegPictureCallbackTime = 0;
 
-            // reset exposure
-            mParameters.setExposureCompensation(mResetExposure);
-            mCameraDevice.setParameters(mParameters);
         }
     }
 
@@ -1060,8 +1067,6 @@ public class PhotoModule
             animateFlash();
         }
 
-        //save data
-        mResetExposure = mParameters.getExposureCompensation();
         // Set rotation and gps data.
         int orientation;
         // We need to be consistent with the framework orientation (i.e. the
@@ -1072,11 +1077,6 @@ public class PhotoModule
             orientation = mOrientation;
         }
         mJpegRotation = Util.getJpegRotation(mCameraId, orientation);
-        if (mSceneMode == Util.SCENE_MODE_HDR && Util.needSamsungHDRFormat()) {
-            /* samsung actually speficify max range via exposure compinsation */
-            mParameters.setExposureCompensation(mParameters.getMaxExposureCompensation());
-        }
-        mJpegRotation = Util.getJpegRotation(mCameraId, mOrientation);
         mParameters.setRotation(mJpegRotation);
         Location loc = mLocationManager.getCurrentLocation();
         Util.setGpsParameters(mParameters, loc);
@@ -1147,6 +1147,18 @@ public class PhotoModule
                 CameraSettings.KEY_FLASH_MODE, flashMode,
                 CameraSettings.KEY_WHITE_BALANCE, whiteBalance,
                 CameraSettings.KEY_FOCUS_MODE, focusMode);
+        if(Util.needSamsungHDRFormat()){
+            if(mSceneMode == Util.SCENE_MODE_HDR ) {
+                mUI.overrideSettings(CameraSettings.KEY_EXPOSURE,
+                                    ((Integer)mParameters.getMaxExposureCompensation())
+                                    .toString());
+                mParameters.setExposureCompensation(mParameters.getMaxExposureCompensation());
+            } else if(Parameters.SCENE_MODE_AUTO.equals(mSceneMode)) {
+                mUI.overrideSettings(CameraSettings.KEY_EXPOSURE, null);
+                mParameters.setExposureCompensation(CameraSettings.readExposure(mPreferences));
+            }
+            mCameraDevice.setParameters(mParameters);
+        }
     }
 
     private void loadCameraPreferences() {
@@ -1902,7 +1914,9 @@ public class PhotoModule
         int max = mParameters.getMaxExposureCompensation();
         int min = mParameters.getMinExposureCompensation();
         if (value >= min && value <= max) {
-            mParameters.setExposureCompensation(value);
+            if(!(mSceneMode == Util.SCENE_MODE_HDR && Util.needSamsungHDRFormat())) {
+                mParameters.setExposureCompensation(value);
+            }
         } else {
             Log.w(TAG, "invalid exposure range: " + value);
         }

SlimRoms/packages_apps_Gallery2

src/com/android/camera/PhotoModule.java
@@ -256,8 +256,6 @@ public class PhotoModule
     private final Handler mHandler = new MainHandler();
     private PreferenceGroup mPreferenceGroup;
 
-    private int mResetExposure;
-
     private boolean mQuickCapture;
 
     CameraStartUpThread mCameraStartUpThread;
@@ -464,7 +462,6 @@ public class PhotoModule
 
     private void onPreviewStarted() {
         mCameraStartUpThread = null;
-        mResetExposure = mParameters.getExposureCompensation();
         setCameraState(IDLE);
         if (!ApiHelper.HAS_SURFACE_TEXTURE) {
             // This may happen if surfaceCreated has arrived.
@@ -797,7 +794,7 @@ public class PhotoModule
 
         @Override
         public void onPictureTaken(
-                byte [] jpegData, final android.hardware.Camera camera) {
+                final byte [] jpegData, final android.hardware.Camera camera) {
             if (mPaused) {
                 return;
             }
@@ -865,19 +862,17 @@ public class PhotoModule
             if (!mIsImageCaptureIntent) {
                 // Calculate the width and the height of the jpeg.
                 Size s = mParameters.getPictureSize();
-                ExifInterface exif = Exif.getExif(jpegData);
-                int orientation = Exif.getOrientation(exif);
-                int width, height;
-                if ((mJpegRotation + orientation) % 180 == 0 ||
-                        (mSceneMode == Util.SCENE_MODE_HDR &&
-                            Util.needSamsungHDRFormat())) {
+                final ExifInterface exif = Exif.getExif(jpegData);
+                final int orientation = Exif.getOrientation(exif);
+                final int width, height;
+                if ((mJpegRotation + orientation) % 180 == 0 || isSamsungHDR) {
                     width = s.width;
                     height = s.height;
                 } else {
                     width = s.height;
                     height = s.width;
                 }
-                String title = mNamedImages.getTitle();
+                final String title = mNamedImages.getTitle();
                 long date = mNamedImages.getDate();
                 if (title == null) {
                     Log.e(TAG, "Unbalanced name/data pair");
@@ -896,29 +891,35 @@ public class PhotoModule
                     }
 
                     if (isSamsungHDR) {
-                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                        Bitmap bm = Util.decodeYUV422P(jpegData, width, height);
-                        if (mJpegRotation != 0) {
-                            Matrix matrix = new Matrix();
-                            matrix.postRotate(mJpegRotation);
-                            bm = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, true);
-                        }
-                        if (mJpegRotation % 180 != 0) {
-                            int x = height;
-                            int y = width;
-                            width = x;
-                            height = y;
-                        }
+                        final long finalDate = date;
+                        new Thread(new Runnable() {
+                            public void run() {
+                                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                                Bitmap bm = Util.decodeYUV422P(jpegData, width, height);
+                                if (mJpegRotation != 0) {
+                                    Matrix matrix = new Matrix();
+                                    matrix.postRotate(mJpegRotation);
+                                    bm = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, true);
+                                }
 
-                        bm.compress(Bitmap.CompressFormat.JPEG,
-                                    CameraSettings.getJpegQualityIntValue(mPreferences),
-                                    baos);
+                                bm.compress(Bitmap.CompressFormat.JPEG,
+                                            CameraSettings.getJpegQualityIntValue(mPreferences),
+                                            baos);
 
-                        jpegData = baos.toByteArray();
-                    }
-                    mActivity.getMediaSaveService().addImage(
+                                boolean rotated = (mJpegRotation % 180) != 0;
+
+                                mActivity.getMediaSaveService().addImage(
+                                    baos.toByteArray(), title, finalDate, mLocation,
+                                    rotated ? height : width, rotated ? width : height,
+                                    orientation,  exif, mOnMediaSavedListener, mContentResolver);
+                            }
+                        }).start();
+
+                    } else {
+                        mActivity.getMediaSaveService().addImage(
                             jpegData, title, date, mLocation, width, height,
                             orientation, exif, mOnMediaSavedListener, mContentResolver);
+                    }
                 }
             } else {
                 mJpegImageData = jpegData;
@@ -941,9 +942,6 @@ public class PhotoModule
                     + mJpegCallbackFinishTime + "ms");
             mJpegPictureCallbackTime = 0;
 
-            // reset exposure
-            mParameters.setExposureCompensation(mResetExposure);
-            mCameraDevice.setParameters(mParameters);
         }
     }
 
@@ -1060,8 +1058,6 @@ public class PhotoModule
             animateFlash();
         }
 
-        //save data
-        mResetExposure = mParameters.getExposureCompensation();
         // Set rotation and gps data.
         int orientation;
         // We need to be consistent with the framework orientation (i.e. the
@@ -1072,11 +1068,6 @@ public class PhotoModule
             orientation = mOrientation;
         }
         mJpegRotation = Util.getJpegRotation(mCameraId, orientation);
-        if (mSceneMode == Util.SCENE_MODE_HDR && Util.needSamsungHDRFormat()) {
-            /* samsung actually speficify max range via exposure compinsation */
-            mParameters.setExposureCompensation(mParameters.getMaxExposureCompensation());
-        }
-        mJpegRotation = Util.getJpegRotation(mCameraId, mOrientation);
         mParameters.setRotation(mJpegRotation);
         Location loc = mLocationManager.getCurrentLocation();
         Util.setGpsParameters(mParameters, loc);
@@ -1147,6 +1138,17 @@ public class PhotoModule
                 CameraSettings.KEY_FLASH_MODE, flashMode,
                 CameraSettings.KEY_WHITE_BALANCE, whiteBalance,
                 CameraSettings.KEY_FOCUS_MODE, focusMode);
+        if (Util.needSamsungHDRFormat()){
+            if (mSceneMode == Util.SCENE_MODE_HDR) {
+                mUI.overrideSettings(CameraSettings.KEY_EXPOSURE,
+                        String.valueOf(mParameters.getMaxExposureCompensation()));
+                mParameters.setExposureCompensation(mParameters.getMaxExposureCompensation());
+            } else {
+                mUI.overrideSettings(CameraSettings.KEY_EXPOSURE, null);
+                mParameters.setExposureCompensation(CameraSettings.readExposure(mPreferences));
+            }
+            mCameraDevice.setParameters(mParameters);
+        }
     }
 
     private void loadCameraPreferences() {
@@ -1902,7 +1904,9 @@ public class PhotoModule
         int max = mParameters.getMaxExposureCompensation();
         int min = mParameters.getMinExposureCompensation();
         if (value >= min && value <= max) {
-            mParameters.setExposureCompensation(value);
+            if (mSceneMode != Util.SCENE_MODE_HDR || !Util.needSamsungHDRFormat()) {
+                mParameters.setExposureCompensation(value);
+            }
         } else {
             Log.w(TAG, "invalid exposure range: " + value);
         }