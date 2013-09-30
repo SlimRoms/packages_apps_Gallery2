84e5dc851e576b784d3996d57999f5d72d5cb8e0
Jorge Ruesga <jorge@ruesga.com>
2013-09-30 20:33:08 +0200

    Gallery: Fix filmstrip source index
    Change-Id: I0213871f47fb386ddce5728f0079eb7884e6d9d4
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- src/com/android/gallery3d/app/PhotoPage.java
+++ src/com/android/gallery3d/app/PhotoPage.java
@@ -604,6 +604,13 @@ public abstract class PhotoPage extends ActivityState implements
         return mIsActive && !mPhotoView.canUndo();
     }
 
+    /**
+     * Returns the start source index of the album
+     */
+    protected int getStartSourceIndex() {
+        return 0;
+    }
+
     @Override
     public boolean canDisplayBottomControl(int control) {
         if (mCurrentPhoto == null) {
@@ -1503,12 +1510,12 @@ public abstract class PhotoPage extends ActivityState implements
 
         @Override
         public int size() {
-            return mMediaSet != null ? mMediaSet.getMediaItemCount() : 1;
+            return mMediaSet != null ? mMediaSet.getMediaItemCount() - getStartSourceIndex() : 1;
         }
 
         @Override
         public int setIndex() {
-            return mModel.getCurrentIndex();
+            return mModel.getCurrentIndex() - getStartSourceIndex();
         }
     }
 
